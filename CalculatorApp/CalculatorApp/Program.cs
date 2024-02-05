c836d4b869dc58f3d5f109df64b1abb41caf757a
Alyssa Williams <109011613+alyssaw770@users.noreply.github.com>
2024-02-04 22:01:34 -0600

    code added to get user input and output answer.

--- CalculatorApp/CalculatorApp/Program.cs
+++ CalculatorApp/CalculatorApp/Program.cs
@@ -1,10 +1,50 @@
-﻿namespace CalculatorApp;
+﻿using static System.Collections.Specialized.BitVector32;
+
+namespace CalculatorApp;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+
+        int num1;
+        int num2;
+        int answer;
+        string selection;
+
+        Console.WriteLine("Welcome to The Calculator App!");
+
+        Console.WriteLine("Enter you first number:");
+        num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter another number:");
+        num2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Select a for addition, s for subtraction, m for multiplication, and d for division.");
+
+        selection = Console.ReadLine();
+
+
+        if (selection == "a")
+        {
+            answer = num1 + num2;
+        }
+        else if (selection == "s")
+        {
+            answer = num1 - num2;
+        }
+        else if (selection == "m")
+        {
+            answer = num1 * num2;
+        }
+        else
+        {
+            answer = num1 / num2;
+        }
+
+        Console.WriteLine("The answer is " + answer);
+
+        Console.ReadKey();
     }
 }
 
